535aa0ff78a527990cde507c5f1f0b4bd2f7fddd
punchlemon <tsukiwahigashini0410@gmail.com>
2024-09-30 06:40:23 +0900

    change directory and rename file

---
+++ srcs/analysis/ft_string/create_str.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_str.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 23:27:23 by retanaka          #+#    #+#             */
+/*   Updated: 2024/09/23 16:39:27 by retanaka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_memory.h"
+#include "ft_string.h"
+
+t_str	*create_str(char *src)
+{
+	t_str	*str;
+
+	str = ft_calloc(sizeof(t_str));
+	if (!str)
+		return (NULL);
+	str->len = ft_strlen(src);
+	str->cap = 1;
+	update_cap(str);
+	str->data = ft_calloc(sizeof(char) * str->cap);
+	if (!str->data)
+		return (str->cap = 0, str->len = 0, free(str), NULL);
+	ft_memcpy(str->data, src, str->len);
+	return (str);
+}
+
+t_str	*create_str_len(char *src, size_t len)
+{
+	t_str	*str;
+
+	str = ft_calloc(sizeof(t_str));
+	if (!str)
+		return (NULL);
+	str->len = len;
+	str->cap = 1;
+	update_cap(str);
+	str->data = ft_calloc(sizeof(char) * str->cap);
+	if (!str->data)
+		return (str->cap = 0, str->len = 0, free(str), NULL);
+	ft_memcpy(str->data, src, str->len);
+	return (str);
+}
