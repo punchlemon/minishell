36b2a3daff18b189d964f5d94319b2b93f8bdbba
punchlemon <tsukiwahigashini0410@gmail.com>
2024-09-30 06:55:11 +0900

    rename file mem_zero -> ft_memzero

--- srcs/analysis/ft_memory/ft_memzero.c
+++ srcs/analysis/ft_memory/ft_memzero.c
@@ -1,38 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mem_zero.c                                         :+:      :+:    :+:   */
+/*   ft_memzero.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 17:46:04 by retanaka          #+#    #+#             */
-/*   Updated: 2024/09/12 16:41:23 by retanaka         ###   ########.fr       */
+/*   Updated: 2024/09/23 19:46:48 by retanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdint.h>
 
-void	mem_zero(void *dst, size_t bytes)
+void	ft_memzero(char *dst, size_t n)
 {
-	size_t	n64;
-
-	n64 = bytes >> 3;
-	while (n64--)
-	{
-		*(uint64_t *)dst = 0;
-		dst += 8;
-	}
-	if (bytes & 0b100)
-	{
-		*(uint32_t *)dst = 0;
-		dst += 4;
-	}
-	if (bytes & 0b10)
-	{
-		*(uint16_t *)dst = 0;
-		dst += 2;
-	}
-	if (bytes & 0b1)
-		*(uint8_t *)dst = 0;
+	while (n--)
+		dst[n] = 0;
 }
+
+// void	mem_zero(void *dst, size_t bytes)
+// {
+// 	size_t	n64;
+//
+// 	if (!dst || !bytes)
+// 		return ;
+// 	n64 = bytes >> 3;
+// 	while (n64--)
+// 	{
+// 		*(uint64_t *)dst = 0;
+// 		dst += 8;
+// 	}
+// 	if (bytes & 0b100)
+// 	{
+// 		*(uint32_t *)dst = 0;
+// 		dst += 4;
+// 	}
+// 	if (bytes & 0b10)
+// 	{
+// 		*(uint16_t *)dst = 0;
+// 		dst += 2;
+// 	}
+// 	if (bytes & 0b1)
+// 		*(uint8_t *)dst = 0;
+// }
