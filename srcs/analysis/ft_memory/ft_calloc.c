265f1b50602014d1793698769c266c0a29ff2565
punchlemon <tsukiwahigashini0410@gmail.com>
2024-09-30 06:53:14 +0900

    add ft_calloc.c

---
+++ srcs/analysis/ft_memory/ft_calloc.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 22:59:26 by retanaka          #+#    #+#             */
+/*   Updated: 2024/09/23 21:47:52 by retanaka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_memory.h"
+#include <stdlib.h>
+
+void	*ft_calloc(size_t n)
+{
+	void	*p;
+
+	p = malloc(n);
+	if (!p)
+		return (NULL);
+	ft_memzero(p, n);
+	return (p);
+}
