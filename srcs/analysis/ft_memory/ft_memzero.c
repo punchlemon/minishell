84365f0efeb7713e959ce80e34ea54d55b5caa26
punchlemon <tsukiwahigashini0410@gmail.com>
2024-09-30 06:57:14 +0900

    obey the strict aliasing rules

--- srcs/analysis/ft_memory/ft_memzero.c
+++ srcs/analysis/ft_memory/ft_memzero.c
@@ -6,7 +6,7 @@
 /*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 17:46:04 by retanaka          #+#    #+#             */
-/*   Updated: 2024/09/23 19:46:48 by retanaka         ###   ########.fr       */
+/*   Updated: 2024/09/30 06:55:28 by retanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,3 @@ void	ft_memzero(char *dst, size_t n)
 	while (n--)
 		dst[n] = 0;
 }
-
-// void	mem_zero(void *dst, size_t bytes)
-// {
-// 	size_t	n64;
-//
-// 	if (!dst || !bytes)
-// 		return ;
-// 	n64 = bytes >> 3;
-// 	while (n64--)
-// 	{
-// 		*(uint64_t *)dst = 0;
-// 		dst += 8;
-// 	}
-// 	if (bytes & 0b100)
-// 	{
-// 		*(uint32_t *)dst = 0;
-// 		dst += 4;
-// 	}
-// 	if (bytes & 0b10)
-// 	{
-// 		*(uint16_t *)dst = 0;
-// 		dst += 2;
-// 	}
-// 	if (bytes & 0b1)
-// 		*(uint8_t *)dst = 0;
-// }
