5da5203b4781d09641ef6bc3affc8d6b8649b5bf
punchlemon <tsukiwahigashini0410@gmail.com>
2024-10-17 13:46:57 +0900

    fix main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 13:28:08 by retanaka          #+#    #+#             */
-/*   Updated: 2024/10/16 16:41:36 by retanaka         ###   ########.fr       */
+/*   Updated: 2024/10/17 13:46:22 by retanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,35 +32,35 @@
 // 	exit (0);
 // }
 
-static void	execute(t_exec_plan *exec_plan, char **environ)
+static void	execute(t_cond *conds, char **environ)
 {
 	size_t	i;
 	int		status;
 
 	i = 0;
-	while (i < exec_plan->count)
+	while (conds[i].type != TAIL)
 	{
-		if (exec_plan->conds[i].type == CMD)
-			exe(exec_plan->conds[i].pipeline[0].normal_cmd->argv, environ, &status);
-		else if (exec_plan->conds[i].type == AND)
+		if (conds[i].type == HEAD)
+			exe(conds[i].pipeline[0].normal_cmd->words, environ, &status);
+		else if (conds[i].type == AND)
 		{
 			if (!status)
-				exe(exec_plan->conds[i].pipeline[0].normal_cmd->argv, environ, &status);
+				exe(conds[i].pipeline[0].normal_cmd->words, environ, &status);
 		}
 		else
 		{
 			if (status)
-				exe(exec_plan->conds[i].pipeline[0].normal_cmd->argv, environ, &status);
+				exe(conds[i].pipeline[0].normal_cmd->words, environ, &status);
 		}
 		i++;
 	}
-	delete_exec_plan(exec_plan);
+	delete_conds(conds);
 }
 
 int	main(int argc, char **argv, char **environ)
 {
-	char		*line;
-	t_exec_plan	*exec_plan;
+	char	*line;
+	t_cond	*conds;
 
 	(void)argc;
 	(void)argv;
@@ -74,9 +74,9 @@ int	main(int argc, char **argv, char **environ)
 			// add_history(line);
 			// if (!ft_strcmp(line, "exit"))
 			// 	cleanup_and_exit(line);
-			exec_plan = analysis(line);
-			if (exec_plan)
-				execute(exec_plan, environ);
+			conds = analysis(line);
+			if (conds)
+				execute(conds, environ);
 		}
 		free(line);
 	}
