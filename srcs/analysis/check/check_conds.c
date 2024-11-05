0a828779d5f9c6237a58f63a172952a44f79fde8
punchlemon <tsukiwahigashini0410@gmail.com>
2024-11-05 09:06:49 +0900

    fix and_if or_if

--- srcs/analysis/check/check_conds.c
+++ srcs/analysis/check/check_conds.c
@@ -6,7 +6,7 @@
 /*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 21:05:36 by retanaka          #+#    #+#             */
-/*   Updated: 2024/11/05 08:19:54 by retanaka         ###   ########.fr       */
+/*   Updated: 2024/11/05 09:06:11 by retanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,9 +44,11 @@ int	check_conds(const t_tkn *tkns, const size_t t_end)
 	if (!check_cond(tkns, tmp))
 		return (0);
 	t_i = tmp;
-	while (t_i < (t_end - 1))
+	while (t_i < t_end)
 	{
 		if (!type_is_and_or(tkns[t_i++].type))
+			return (0);
+		if (t_i >= t_end)
 			return (0);
 		tmp = count_tkns_for_cond(&tkns[t_i], t_end - t_i);
 		if (!check_cond(&tkns[t_i], tmp))
