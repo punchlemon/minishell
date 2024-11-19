ae3c374798305de9c24db3005a0bf72c839aeff9
Hayanobu Nakayama <hnakayam@c4r3s2.42tokyo.jp>
2024-11-19 14:57:56 +0900

    fix; return status

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hnakayam <hnakayam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 13:28:08 by retanaka          #+#    #+#             */
-/*   Updated: 2024/11/19 12:59:51 by hnakayam         ###   ########.fr       */
+/*   Updated: 2024/11/19 14:56:52 by hnakayam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,26 @@
 #include "minishell.h"
 #include "sig.h"
 
-static void	execute(t_cond *conds, t_env *env)
+static void	execute(t_cond *conds, t_env *env, int *status)
 {
 	size_t	i;
-	int		status;
 
 	i = 0;
-	status = 0;
 	while (conds[i].type != TAIL)
 	{
 		if (conds[i].type == HEAD)
 		{
-			status = exe_cmds(conds[i].cmds, env, &status);
+			*status = exe_cmds(conds[i].cmds, env, status);
 		}
 		else if (conds[i].type == AND_IF)
 		{
-			if (!status)
-				status = exe_cmds(conds[i].cmds, env, &status);
+			if (!(*status))
+				*status = exe_cmds(conds[i].cmds, env, status);
 		}
 		else
 		{
 			if (status)
-				status = exe_cmds(conds[i].cmds, env, &status);
+				*status = exe_cmds(conds[i].cmds, env, status);
 		}
 		i++;
 	}
@@ -48,6 +46,7 @@ int	main(int argc, char **argv, char **environ)
 	char	*line;
 	t_cond	*conds;
 	t_env	*env;
+	int		status;
 
 	env = make_env_list(environ);
 	(void)argc;
@@ -64,7 +63,7 @@ int	main(int argc, char **argv, char **environ)
 			set_exec_handler(false);
 			conds = analysis(line);
 			if (conds)
-				execute(conds, env);
+				execute(conds, env, &status);
 		}
 		free(line);
 	}
