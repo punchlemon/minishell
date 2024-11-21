cfe97dda921eb29a5f7fa443c9b6459839eb291e
punchlemon <tsukiwahigashini0410@gmail.com>
2024-11-21 17:08:47 +0900

    expand empty

--- srcs/execution/excute.c
+++ srcs/execution/excute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   excute.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hnakayam <hnakayam@student.42.fr>          +#+  +:+       +#+        */
+/*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 14:17:03 by hnakayam          #+#    #+#             */
-/*   Updated: 2024/11/21 15:59:44 by hnakayam         ###   ########.fr       */
+/*   Updated: 2024/11/21 17:06:32 by retanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -258,6 +258,26 @@ int	create_word(char **pp, t_tkn *tkns, t_env *env, char *st)
 	return (1);
 }
 
+int	is_env_variable(const char *head, const char *tail)
+{
+	size_t	len;
+	size_t	i;
+
+	len = tail - head;
+	if (*head != '$' || len < 2)
+		return (0);
+	if (head[1] != '_' && ft_isalpha(head[1]))
+		return (0);
+	i = 1;
+	while (i < len)
+	{
+		if (!ft_is_charactor(head[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	store_cmd(t_cmd *cmd, t_tkn *tkns, t_env *env, char *st)
 {
 	size_t	t_i;
@@ -274,19 +294,25 @@ int	store_cmd(t_cmd *cmd, t_tkn *tkns, t_env *env, char *st)
 			cmd->reds[r_i].type = tkns[t_i++].type;
 			cmd->reds[r_i].file_fd = -1;
 			cmd->reds[r_i].std_target_fd = -1;
-			if (!create_word(&(cmd->reds[r_i].target), &tkns[t_i], env, st))
+			if (is_env_variable(tkns[t_i].head, tkns[t_i].tail))
+				cmd->reds[r_i].target = NULL;
+			else if (!create_word(&(cmd->reds[r_i].target), &tkns[t_i], env, st))
 				return (cmd->reds[r_i].type = TAIL, delete_cmd_exe(cmd), 0);
 			r_i++;
 		}
 		else
 		{
-			if (!create_word(&(cmd->words[w_i]), &tkns[t_i], env, st))
+			if (is_env_variable(tkns[t_i].head, tkns[t_i].tail))
+				w_i--;
+			else if (!create_word(&(cmd->words[w_i]), &tkns[t_i], env, st))
 				return (delete_cmd_exe(cmd), 0);
 			w_i++;
 		}
 		t_i += count_tkns_for_word(&tkns[t_i]);
 	}
 	return (1);
+	cmd->words[w_i] = NULL;
+	cmd->reds[r_i].type = TAIL;
 }
 
 t_cmd	*expand_cmd(t_cmd *cmd, t_tkn *tkns, t_env *env, int status)
@@ -302,11 +328,9 @@ t_cmd	*expand_cmd(t_cmd *cmd, t_tkn *tkns, t_env *env, int status)
 	cmd->words = malloc(sizeof(char *) * (words_len + 1));
 	if (!cmd->words)
 		return (free(st), NULL);
-	cmd->words[words_len] = NULL;
 	cmd->reds = malloc(sizeof(t_red) * (reds_len + 1));
 	if (!cmd->reds)
 		return (free(st), free(cmd->words), NULL);
-	cmd->reds[reds_len].type = TAIL;
 	if (!store_cmd(cmd, tkns, env, st))
 		return (free(st), NULL);
 	return (free(st), cmd);
