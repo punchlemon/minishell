23918e143d2ee7d6e9e4bf701ae95deb3057d184
punchlemon <tsukiwahigashini0410@gmail.com>
2024-11-29 20:12:08 +0900

    resolve #24

--- srcs/execution/store_cmd.c
+++ srcs/execution/store_cmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   store_cmd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hnakayam <hnakayam@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: retanaka <retanaka@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 16:09:54 by retanaka          #+#    #+#             */
-/*   Updated: 2024/11/29 11:43:59 by hnakayam         ###   ########.fr       */
+/*   Updated: 2024/11/29 20:08:22 by retanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,8 +44,6 @@ int	store_red(t_red *red, t_tkn *tkn, t_env *env, char *st)
 		if (!create_heredoc(red, tkn))
 			return (0);
 	}
-	else if (!create_word(&(red->target), tkn, env, st)) // koko iran
-		return (0);
 	else if (is_env_variable(tkn->head, tkn->tail)
 		&& !get_value(tkn->head, env, st))
 	{
